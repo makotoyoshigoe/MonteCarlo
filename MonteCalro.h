dc1e3d4f2b4b3d1d1b46791791144945eca78401
makotoyoshigoe <makotoyoshigoe@gmail.com>
2021-12-30 03:25:47 +0900

    Add&Update file

--- MonteCalro.h
+++ MonteCalro.h
@@ -1,3 +1,5 @@
+#include<math.h>
+
 class MonteCarlo{
     private:
         double R; //半径
@@ -10,3 +12,32 @@ class MonteCarlo{
         int cnt_point(); // 円内にある点を数える
         double calc_pi(); // 円周率を計算
 };
+
+MonteCarlo::MonteCarlo(int N){
+    this->n = N;
+    this->R = 0.5;
+}
+
+double MonteCarlo::gen_rand(){
+    return (double)rand() / RAND_MAX; //乱数を生成
+}
+
+int MonteCarlo::judge_within_circle(){
+    double x = gen_rand();
+    double y = gen_rand();
+    double r = pow(x-R, 2) + pow(y-R, 2); //三平方の定理
+    if(r <= R*R) return 1;
+    else return 0;
+}
+
+int MonteCarlo::cnt_point(){
+    int cnt = 0;
+    for(int i = 0; i < n; i++) cnt += judge_within_circle();
+    return cnt;
+}
+
+double MonteCarlo::calc_pi(){
+    double A = (double)cnt_point() / (double)n;
+    double pi = A / (R*R);
+    return pi;
+}
--- main.cpp
+++ main.cpp
@@ -4,37 +4,7 @@
 #include<iostream>
 #include<time.h>
 #include<stdlib.h>
-#include<math.h>
 #include"MonteCalro.h"
-
-MonteCarlo::MonteCarlo(int N){
-    this->n = N;
-    this->R = 0.5;
-}
-
-double MonteCarlo::gen_rand(){
-    return (double)rand() / RAND_MAX; //乱数を生成
-}
-
-int MonteCarlo::judge_within_circle(){
-    double x = gen_rand();
-    double y = gen_rand();
-    double r = pow(x-R, 2) + pow(y-R, 2); //三平方の定理
-    if(r <= R*R) return 1;
-    else return 0;
-}
-
-int MonteCarlo::cnt_point(){
-    int cnt = 0;
-    for(int i = 0; i < n; i++) cnt += judge_within_circle();
-    return cnt;
-}
-
-double MonteCarlo::calc_pi(){
-    double A = (double)cnt_point() / (double)n;
-    double pi = A / (R*R);
-    return pi;
-}
 
 // // メイン処理
 int main(void){
